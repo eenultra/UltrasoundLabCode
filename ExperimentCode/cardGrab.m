85e7ae1c203c32701d6bd90f8ec4b45eb2516e2a
jrmclaughlan <j.r.mclaughlan@leeds.ac.uk>
2019-04-16 15:16:07 +0100

    finishing code

--- ExperimentCode/cardGrab.m
+++ ExperimentCode/cardGrab.m
@@ -1,7 +1,7 @@
 %%
 fs         = 2500;                      % sample frequency in MHz
 noChannels = 2;                         % active channel number
-aqTime     = 5;                         % approx total aquisition time in us
+aqTime     = 1;                         % approx total aquisition time in us
 aqInt      = round((aqTime * fs)/1024); % approx number of mem blocks needed for aqTime
 chDR       = [200 200];                 % [Ch0 Ch1] dynamic range in mV
 chIM       = [1 1];                     % channel input impedance, 1 - 50Ohm, 0 - 1MOhm
@@ -13,7 +13,7 @@ daqStSingleAq(aqInt,noChannels,fs,chDR,chIM,TrigType); % check card is powered a
 global cardInfo
 
 %%
-nPulse = 10;
+nPulse = 100;
 avDAT0 = zeros(cardInfo.setMemsize,nPulse);
 
 for k=1:nPulse
@@ -23,6 +23,10 @@ end
 
 figure(4)
 plot(t,mean(avDAT0,2));
+
+%%
+daqEnSingleAq
+
 
 
 
