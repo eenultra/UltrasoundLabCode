6f835528dd995b90098f8d2b3d2d9b8e703b8e20
jrmclaughlan <j.r.mclaughlan@leeds.ac.uk>
2019-06-17 11:00:03 +0100

    Create cncHIFU.m
    New code for using CNC, HIFU transducers, Picoscope and mem hydro

---
+++ ExperimentCode/cncHIFU.m
@@ -0,0 +1,35 @@
+% Beamplotting software for HIFU transducers and Power amps
+% Using Pico USB scope for aqui
+
+% James Mclaughlan
+% Jun 2019
+
+%% Init hardware
+
+global CNC 
+
+% General CNC defaults
+CNC.SoftwareLimits.X.Values = [0 0];
+CNC.SoftwareLimits.X.Enabled = false;
+CNC.SoftwareLimits.Y.Values = [0 0];
+CNC.SoftwareLimits.Y.Enabled = false;
+CNC.SoftwareLimits.Z.Values = [0 0];
+CNC.SoftwareLimits.Z.Enabled = false;
+CNC.SoftwareLimits.R.Values = [-380 380]; 
+CNC.SoftwareLimits.R.Enabled = false;
+
+disp('Initializing CNC...') 
+
+CNC_OpenConnection('COM3');
+CNC_EnableDrives();
+
+CNC_Home();
+CNC_Status();
+disp(['Current Position: ' num2str(CNC_CurrentPosition()) '     Commanded Position: ' num2str(CNC_CommandedPosition())])
+
+agopen % check the COM port, usually COM4 using RS232 - USB cable
+
+%%
+
+
+
