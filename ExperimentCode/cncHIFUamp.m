00fad074da749e0286801b7d50a0ec6ebb2c3c61
Ultrasound Array Research Platform <uarp@astroeq.co.uk>
2020-02-28 15:54:39 +0000

    Code changes for use

--- ExperimentCode/cncHIFUamp.m
+++ ExperimentCode/cncHIFUamp.m
@@ -20,27 +20,27 @@ CNC.SoftwareLimits.R.Enabled = false;
 
 disp('Initializing CNC...') 
 
-CNC_OpenConnection('COM6');
+CNC_OpenConnection('COM8');
 CNC_EnableDrives();
 
 CNC_Home();
 CNC_Status();
 disp(['Current Position: ' num2str(CNC_CurrentPosition()) '     Commanded Position: ' num2str(CNC_CommandedPosition())])
 
-scope      = ULab.Scope.KEYSIGHT_SCOPE('scope', 'tcp4', '192.168.42.100');
+scope      = ULab.Scope.KEYSIGHT_SCOPE('scope', 'tcp4', '192.168.42.111');
 %% Agilent Sig Gen configure
 sg = ULab.SigGen.KEYSIGHT_3360xx('FunctionGenerator', 'TCP4', '192.168.42.106');
 
 % Waveform config
 sg.Function = 'sin';
-sg.Frequency = 1.1e6;
-sg.Amplitude = 0.150; %% DO NOT EXCEED 480mV!
+sg.Frequency = 3.3e6;
+sg.Amplitude = 0.025; %% DO NOT EXCEED 480mV!
 sg.Offset = 0;
 sg.Phase = 0;
 
 % Burst config
 sg.Burst_Enabled = true;
-sg.Burst_Cycles = 10;
+sg.Burst_Cycles = 30;
 sg.Burst_Mode = 'Triggered';
 
 % Upload parameters
@@ -51,7 +51,7 @@ sg.disable;
 
 %% CNC position for focal point
 
-cncFocus = [2, 297, 30.5]; % this is usually pre-aligned
+cncFocus = [65,135,-190]; % this is usually pre-aligned
 
 %% Scope config
 dat        = scope.downloadData('segments', 'all'); % downloads what's on the scope, just to get buffer length
@@ -60,18 +60,18 @@ clear dat;
 
 %% Set scan ranges and data Aqu
 
-aScanRange = (-10:0.5:10); % in mm range;
-bScanRange = (-5:0.2:5); % in mm range; 
+aScanRange = (-5:0.25:5); % in mm range;
+bScanRange = (-10:0.5:10); % in mm range; 
 
-hydroCal = 0.353; %V/MPa at 3.3MHz from calibration sheet 
+%hydroCal = 0.353; %V/MPa at 3.3MHz from calibration sheet 
 
-fName = '281024_1p1MHz_150mV_6MPa_H102';
+fName = '200219_3p3MHz_H102i_R1';
 
 figure(1);figure(2);figure(3)
 
 %% Peform Scan
 
-plane = ['XY';'YZ'];
+plane = ['XZ';'YZ'];
 sPlane = size(plane);
 
 input('Ready?');
@@ -84,7 +84,6 @@ for k = 1:sPlane(1)
     
     for i=1:length(aScanRange)
         for j=1:length(bScanRange)
-            scope.clear
 
             if strcmp(plane(k,:),'XY') == 1
             disp([num2str(bScanRange(j)+cncFocus(1)) 'mm, ' num2str(aScanRange(i)+cncFocus(2)) 'mm, ' num2str(cncFocus(3)) 'mm'])     
@@ -94,8 +93,13 @@ for k = 1:sPlane(1)
             disp([num2str(cncFocus(1)) 'mm, ' num2str(aScanRange(i)+cncFocus(2)) 'mm, ' num2str(bScanRange(j)+cncFocus(3)) 'mm'])     
             CNC_MovePositionLinear(cncFocus(1), aScanRange(i)+cncFocus(2), bScanRange(j)+cncFocus(3), 0, true);  
             fileName = [fName '_YZ'];
+            elseif strcmp(plane(k,:),'XZ') == 1
+            disp([num2str(cncFocus(1)) 'mm, ' num2str(aScanRange(i)+cncFocus(2)) 'mm, ' num2str(bScanRange(j)+cncFocus(3)) 'mm'])     
+            CNC_MovePositionLinear(aScanRange(i)+cncFocus(1), cncFocus(2), bScanRange(j)+cncFocus(3), 0, true);  
+            fileName = [fName '_XZ'];
             end
 
+            scope.clear
             pause(0.5);       
 
             sg.enable;
@@ -104,20 +108,21 @@ for k = 1:sPlane(1)
             sg.disable;
 
             vData(:,j,i) = dat.Waveforms(1).Buffers.AmplitudeData{1};
-            %pData(:,j,i) = HydrophoneInverseFilter(dat.Waveforms(2).Buffers.AmplitudeData{1},1/dat.Waveforms(1).XIncrement,2);
-            pData(:,j,i) = dat.Waveforms(2).Buffers.AmplitudeData{1}/hydroCal;
-            pPP(j,i)     = max(pData(50000:end,j,i));
-            pNP(j,i)     = abs(min(pData(50000:end,j,i)));
+            pData(:,j,i) = HydrophoneInverseFilter(dat.Waveforms(2).Buffers.AmplitudeData{1},1/dat.Waveforms(1).XIncrement,2);
+            %pData(:,j,i) = dat.Waveforms(2).Buffers.AmplitudeData{1}/hydroCal;
+            pPP(j,i)     = max(pData(5000:end,j,i));
+            pNP(j,i)     = abs(min(pData(5000:end,j,i)));
 
             figure(1);plot(dat);drawnow;
-            figure(2);imagesc(bScanRange,aScanRange,pPP);title('PPP');colorbar;drawnow;
-            figure(3);imagesc(bScanRange,aScanRange,pNP);title('PNP');colorbar;drawnow;
+            figure(2);imagesc(aScanRange,bScanRange,pPP/1E6);title('PPP');colorbar;drawnow;
+            figure(3);imagesc(aScanRange,bScanRange,pNP/1E6);title('PNP');colorbar;drawnow;
 
         end
     end
 
 tData = dat.Waveforms(2).TimeData;
-CNC_MovePositionLinear(  cncFocus(1), cncFocus(2), cncFocus(3), 0, true);
+%Move hydrophone to a safe position in case TXD drops.
+CNC_MovePositionLinear(65, 50, -190, 0, true);
 disp('Saving...');
 save([fileName '.mat'],'vData','pData','pPP','pNP','tData','aScanRange','bScanRange','cncFocus','-v7.3');
 clear pData pPP pNP vData;
